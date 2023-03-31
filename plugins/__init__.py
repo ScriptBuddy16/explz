3fddae89523870dce02dcfb7397bc8bfe840f5c5
ScriptBuddy16 <89794297+ScriptBuddy16@users.noreply.github.com>
2023-04-01 00:20:21 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -8,9 +8,9 @@ import sys
 
 
 @stark.on_message(filters.command(["start"]) & ~filters.edited)
-async def Start_msg(Client: stark , m: Message):
+async def Start_msg(bot: stark , m: Message):
     await bot.send_photo(
-    m.chat.id,
+    m.chat_id,
     photo="https://telegra.ph/file/cef3ef6ee69126c23bfe3.jpg",
     caption = "**Hi i am All in One Extractor Bot**.\n"
                             "Press **/pw** for **Physics Wallah**..\n\n"
@@ -39,4 +39,4 @@ async def restart_handler(_, m):
 
 @stark.on_message(filters.command(["log"]) & ~filters.edited)
 async def log_msg(bot: stark , m: Message):   
-    await bot.send_document(m.chat.id, "log.txt")
+    await bot.send_document(m.chat_id, "log.txt")
